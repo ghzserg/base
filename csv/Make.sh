d502bd691f8b7b447e04a1b921ca937fcd219562
Namida Verasche <namida.verasche@gmail.com>
2026-02-16 19:56:17 +1300

    More fixes; gitignored the generated files + made Make.sh erase them. Tested with and without native screen on AD5X.

--- csv/Make.sh
+++ csv/Make.sh
@@ -6,3 +6,7 @@ for i in *.csv; do
     python3 translate.py $i ../../translate/
 done
 
+rm ../ff5m_config_native.cfg
+rm ../ff5m_config_off.cfg
+rm ../ad5x_config_native.cfg
+rm ../ad5x_config_off.cfg
